d6381ff3394db764c20d71798084b2d45fd4c200
Gribbleshnibit8 <gribbleshnibit8@gmail.com>
2014-12-04 12:05:31 -0600

    Added initial support for KerbalStuff. Correctly fetches mod info, downloads files (assumes all as zip, as that seems all KerbalStuff supports right now). Modified ModInfo to have a Version field to allow for handling downloads of current versions.

--- KSPModAdmin.Core/Model/ModInfo.cs
+++ KSPModAdmin.Core/Model/ModInfo.cs
@@ -68,32 +68,6 @@ namespace KSPModAdmin.Core.Model
             }
         }
 
-        public string ChangeDate { get; set; }
-
-        public DateTime ChangeDateAsDateTime
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(ChangeDate))
-                    return DateTime.MinValue;
-                else
-                {
-                    DateTime value = DateTime.MinValue;
-                    if (DateTime.TryParse(ChangeDate, out value))
-                        return value;
-                    else
-                        return DateTime.MinValue;
-                }
-            }
-            set
-            {
-                if (value == DateTime.MinValue)
-                    ChangeDate = string.Empty;
-                else
-                    ChangeDate = value.ToString();
-            }
-        }
-
         public string Rating { get; set; }
 
         public string Downloads { get; set; }
@@ -101,6 +75,8 @@ namespace KSPModAdmin.Core.Model
         public string Author { get; set; }
 
         public string ProductID { get; set; }
+
+		public string Version { get; set; }
 
         public bool IsArchive
         {
@@ -170,7 +146,7 @@ namespace KSPModAdmin.Core.Model
 
             Author = string.Empty;
 
-            ProductID = (string.IsNullOrEmpty(ProductID)) ? string.Empty : ProductID;
+            ProductID = string.Empty;
         }
     }
 }
