4e83a691d0ee4fed444622f9b43daca24de28c66
MacTee <mackerbal@mactee.de>
2014-12-11 12:52:08 +0100

    Select Download dialog added.

---
+++ KSPModAdmin.Core/Views/frmSelectDownload.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace KSPModAdmin.Core.Views
+{
+    partial class frmSelectDownload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbLinks = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbLinks
+            // 
+            this.cbLinks.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbLinks.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLinks.FormattingEnabled = true;
+            this.cbLinks.Location = new System.Drawing.Point(26, 44);
+            this.cbLinks.Name = "cbLinks";
+            this.cbLinks.Size = new System.Drawing.Size(403, 21);
+            this.cbLinks.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(422, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please select on of the following links where the mod could be downloaded.";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Image = global::KSPModAdmin.Core.Properties.Resources.delete2;
+            this.btnCancel.Location = new System.Drawing.Point(370, 77);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 25);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.ForeColor = System.Drawing.Color.Black;
+            this.btnOK.Image = global::KSPModAdmin.Core.Properties.Resources.tick;
+            this.btnOK.Location = new System.Drawing.Point(289, 77);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 25);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOK.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // frmSelectDownloadURL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 114);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbLinks);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSelectDownloadURL";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Select Download";
+            this.Load += new System.EventHandler(this.frmSelectDownloadURL_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ComboBox cbLinks;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ KSPModAdmin.Core/Views/frmSelectDownload.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using KSPModAdmin.Core.Utils;
+
+namespace KSPModAdmin.Core.Views
+{
+    public partial class frmSelectDownload : frmBase
+    {
+        public List<DownloadInfo> Links
+        {
+            get
+            {
+                if (cbLinks.Items.Count > 0)
+                    return cbLinks.Items.Cast<DownloadInfo>().ToList();
+                else
+                    return new List<DownloadInfo>();
+            }
+            set
+            {
+                cbLinks.Items.Clear();
+                if (value != null && value.Count > 0)
+                {
+                    foreach (var e in value)
+                        cbLinks.Items.Add(e);
+                    cbLinks.SelectedItem = cbLinks.Items[0];
+                }
+                else
+                    cbLinks.SelectedItem = null;
+            }
+        }
+
+        public DownloadInfo SelectedLink
+        {
+            get
+            {
+                return (DownloadInfo)cbLinks.SelectedItem;
+            }
+        }
+
+
+        public frmSelectDownload()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frmSelectDownloadURL_Load(object sender, EventArgs e)
+        {
+            cbLinks.Select();
+            cbLinks.Focus();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
