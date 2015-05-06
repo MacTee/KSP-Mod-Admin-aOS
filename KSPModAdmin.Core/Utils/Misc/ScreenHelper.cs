9a31749e714a39dcc59f9dda40a705e8f26837bc
MacTee <mackerbal@mactee.de>
2015-05-06 18:06:35 +0200

    SceenHelper.GetResolutionsOnUbuntu() added.
    - New GetResolution function for Linux (not implemented yet!).

--- KSPModAdmin.Core/Utils/Misc/ScreenHelper.cs
+++ KSPModAdmin.Core/Utils/Misc/ScreenHelper.cs
@@ -187,5 +187,32 @@ namespace KSPModAdmin.Core.Utils
 #endif
             return resolutions;
         }
+        
+        private static List<string> GetResolutionsOnUbuntu()
+        {
+            // Use xrandr to get size of screen located at offset (0,0).
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "xrandr";
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            var match = System.Text.RegularExpressions.Regex.Match(output, @"(\d+)x(\d+)\+0\+0");
+            var w = match.Groups[1].Value;
+            var h = match.Groups[2].Value;
+            Rectangle r;
+            r.Width = int.Parse(w);
+            r.Height = int.Parse(h);
+            Console.WriteLine ("Display Size is {0} x {1}", w, h);
+            
+            string newResolution = GetResolutionString(r.Width, r.Height);
+            
+            List<string> resolutions = new List<string>();
+            if (!resolutions.Contains(newResolution))
+                resolutions.Add(newResolution);
+                    
+            return resolutions;
+        }
     }
 }
