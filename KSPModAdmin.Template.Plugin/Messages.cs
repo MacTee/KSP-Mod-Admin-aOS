f190c05aef7aef35e62c76f84901fd6174d2a9fc
4o66 <throwarock@gmail.com>
2015-05-08 12:26:36 -0700

    Template Plugin

---
+++ KSPModAdmin.Template.Plugin/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using KSPModAdmin.Core.Utils.Localization;
+
+namespace KSPModAdmin.Template.Plugin
+{
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+    public class Messages
+    {
+        public static string MSG_PLUGIN_VIEW_TITLE
+        {
+            get
+            {
+                string msg = Localizer.GlobalInstance["MSG_TEMPLATE_VIEW_TITLE"];
+                return (!string.IsNullOrEmpty(msg) ? msg : DEFAULT_MSG_TRANSLATION_VIEW_TITLE).Replace("^", Environment.NewLine);
+            }
+        }
+        private const string DEFAULT_MSG_TRANSLATION_VIEW_TITLE = "Template";
+
+    }
+}
---
+++ KSPModAdmin.Template.Plugin/ucPluginView.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace KSPModAdmin.Template.Plugin
+{
+    partial class ucPluginView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ucPluginView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.MinimumSize = new System.Drawing.Size(450, 400);
+            this.Name = "ucPluginView";
+            this.Size = new System.Drawing.Size(514, 400);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip ttPlugin;
+    }
+}
