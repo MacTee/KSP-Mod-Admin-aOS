02c7c79b8078e2c7d9f8ac8f1528a6bdfcad4c84
MacKerbal <mackerbal@mactee.de>
2015-05-16 15:05:48 +0200

    Initial commit of FlagsTab plugin project.

---
+++ KSPModAdmin.Plugin.FlagsTab/KSPMAPlugin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using KSPModAdmin.Core;
+using KSPModAdmin.Core.Utils;
+using KSPModAdmin.Plugin.FlagsTab.Views;
+using KSPModAdmin.Plugin.FlagsTab.Properties;
+
+namespace KSPModAdmin.Plugin.FlagsTab
+{
+    /// <summary>
+    /// KSP MA plugin container class.
+    /// </summary>
+    public class KSPMAPlugin : IKSPMAPlugin
+    {
+        private TabView[] mMainTabViews = null;
+        private TabView[] mOptionTabViews = new List<TabView>().ToArray(); // Initialize with an empty array if not needed.
+
+
+        /// <summary>
+        /// Name of the plugin.
+        /// </summary>
+        public string Name { get { return "FlagsTab Plugin"; } }
+
+        /// <summary>
+        /// Description of the plugin.
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return "This Plugin adds a tab to manage the Flags of KSP.";
+            }
+        }
+
+        /// <summary>
+        /// Array of TabViews that the mod imports.
+        /// </summary>
+        public TabView[] MainTabViews
+        {
+            get
+            {
+                if (mMainTabViews == null)
+                    mMainTabViews = new[] { new TabView(new ucFlagsView(), Resources.flag_scotland) };
+
+                return mMainTabViews;
+            }
+        }
+
+        /// <summary>
+        /// Array of OptionTabViews that the mod imports.
+        /// </summary>
+        public TabView[] OptionTabViews
+        {
+            get
+            {
+                return mOptionTabViews;
+            }
+        }
+    }
+}
---
+++ KSPModAdmin.Plugin.FlagsTab/Views/ucFlagsView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows.Forms;
+using KSPModAdmin.Core.Views;
+using KSPModAdmin.Plugin.FlagsTab.Controller;
+using KSPModAdmin.Plugin.FlagsTab.Properties;
+
+namespace KSPModAdmin.Plugin.FlagsTab.Views
+{
+    [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Reviewed. Suppression is OK here.")]
+    public partial class ucFlagsView : ucBase
+    {
+        /// <summary>
+        /// Creates a new instance of the ucTranslationView class.
+        /// </summary>
+        public ucFlagsView()
+        {
+            InitializeComponent();
+
+            if (LicenseManager.UsageMode == LicenseUsageMode.Designtime || DesignMode)
+                return;
+
+            FlagsViewController.Initialize(this);
+        }
+
+        #region Event handling
+
+        private void ucPluginView_Load(object sender, EventArgs e)
+        {
+            // do View related init here or in the FlagsViewController.Initialize(...) methode.
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Forces the view to redraw.
+        /// </summary>
+        public override void InvalidateView()
+        {
+            Invalidate();
+            Update();
+            Refresh();
+        }
+
+        /// <summary>
+        /// Gets the Name for the parent TabPage.
+        /// </summary>
+        /// <returns>The Name for the parent TabPage.</returns>
+        public override string GetTabCaption()
+        {
+            return Messages.MSG_FLAGS_VIEW_TITLE;
+        }
+
+        /// <summary>
+        /// Sets the enabled state of some view controls.
+        /// </summary>
+        public void SetEnabledOfAllControls(bool enable)
+        {
+            // Enable/Disable your View Controls here.
+            // Normally when KSP MA calls this methode with enable = false, all controls should be disabled.
+        }
+    }
+}
